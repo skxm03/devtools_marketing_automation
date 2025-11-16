cf0c3c9484fa20e337b28e06b80e06c8d6412242
Saksham Saini <sakshamsainiggn@gmail.com>
2025-11-16 11:49:00 +0530

    frontend files created

--- client/src/components/layout/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-	return <footer>{/* Footer - Copyright, links, etc. */}</footer>;
-};
-
-export default Footer;
--- client/src/components/layout/Sidebar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Sidebar = () => {
-	return <aside>{/* Sidebar - Navigation menu for desktop */}</aside>;
-};
-
-export default Sidebar;
